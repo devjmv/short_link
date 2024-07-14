40af77fe6b47b00c0a3acdfa2a317d7ab54948df
devjmv <javier161890@gmail.com>
2024-07-14 21:12:22 +0200

    integrate bootstrap 5.1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import './assets/main.css'
 
 import { createApp } from 'vue'
@@ -12,3 +13,5 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+import "bootstrap/dist/js/bootstrap.min";
