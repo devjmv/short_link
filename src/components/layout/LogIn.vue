3557554ff90ae56af708d7ea89ec5e304167f414
devjmv <javier161890@gmail.com>
2024-09-13 01:29:17 +0200

    add LogIn and LoginForm

--- src/components/layout/LogIn.vue
+++ src/components/layout/LogIn.vue
@@ -1,14 +1,48 @@
 <script setup>
+import { Dialog, DialogPanel, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import { ref } from 'vue';
+import LoginForm from './LoginForm.vue';
+
 const props = defineProps({
     hidde: String
 })
+
+const loginChange = ref(false)
+
+const modificarLogin = () => {
+    loginChange.value = !loginChange.value
+}
+
 </script>
 <template>
     <div v-if="props.hidde" class="hidden lg:flex lg:flex-1 lg:justify-end">
-        <a href="#" class="text-sm font-semibold leading-6 text-dark dark:text-light">Log in <span
+        <a @click="modificarLogin" href="#" class="text-sm font-semibold leading-6 text-dark dark:text-light">Log in <span
                 aria-hidden="true">&rarr;</span></a>
     </div>
     <a href="#" v-if="!props.hidde" @click="modificarLogin" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
         Log in
     </a>
+    <TransitionRoot as="login" :show="loginChange">
+        <Dialog class="relative z-10" @close="modificarLogin">
+            <TransitionChild as="login" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
+                leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+                <div class="fixed inset-0 bg-bgdark bg-opacity-75 transition-opacity" />
+            </TransitionChild>
+
+            <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center">
+                    <TransitionChild as="login" enter="ease-out duration-300"
+                        enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
+                        leave-from="opacity-100 translate-y-0 sm:scale-100"
+                        leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+                        <DialogPanel
+                            class="relative transform overflow-hidden rounded-lg shadow-xl transition-all ">
+                            <LoginForm />
+                        </DialogPanel>
+                    </TransitionChild>
+                </div>
+            </div>
+        </Dialog>
+    </TransitionRoot>
 </template>
---
+++ src/components/layout/LoginForm.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue'
+
+const textAlert = ref('')
+
+async function login() {
+}
+
+</script>
+<template>
+    <div class="w-full max-w-sm p-8 px-12 bg-light rounded-md shadow-md">
+        <div class="flex items-center justify-center">
+            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+            <span class="font-gloria text-2xl ml-3">LogIn to Short Link</span>
+        </div>
+
+        <form class="mt-4" @submit.prevent="login">
+            <div class="flex items-center justify-center mt-4">
+                <p class="block text-sm text-verdigris fontme" href="#">Log in with your favorite social provider to get started:</p>
+            </div>
+            <div v-if="textAlert != ''"
+                class="mt-4 font-regular relative block w-full rounded-lg bg-pink-500 p-4 text-base leading-5 text-white opacity-100"
+                data-dismissible="alert">
+                <div class="mr-12">{{ textAlert }}</div>
+            </div>
+
+            <div class="mt-6">
+                <button type="submit"
+                    class="w-full px-4 py-2 text-sm text-center text-white bg-primary rounded-md focus:outline-none hover:bg-secondary hover:text-dark">
+                    Google
+                </button>
+            </div>
+            <div class="mt-2">
+                <button type="submit"
+                    class="w-full px-4 py-2 text-sm text-center text-white bg-primary rounded-md focus:outline-none hover:bg-secondary hover:text-dark">
+                    GitHub
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
