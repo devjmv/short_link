bbe1bf09ed0f4f908807121bdcc7c5e38dce97b0
devjmv <javier161890@gmail.com>
2024-09-13 00:52:32 +0200

    updated CardLink and ClientView

--- src/components/view/client/CardLink.vue
+++ src/components/view/client/CardLink.vue
@@ -11,21 +11,20 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="mb-6 rounded-lg bg-white p-6">
+    <div class="mb-6 rounded-lg bg-light dark:bg-dark p-6">
         <div class="flex items-center justify-between">
             <div class="flex items-center">
                 <div>
-                    <a :href="props.short" class="font-semibold text-gray-900 hover:text-primary">{{ props.short }}</a>
-                    <a :href="props.destination" class="block text-xs font-normal text-gray-500 hover:text-dark">{{ props.destination }}</a>
+                    <a :href="props.short" class="font-semibold text-gray-900 dark:text-light hover:text-primary">{{ props.short }}</a>
+                    <a :href="props.destination" class="w-1/2 text-clip overflow-hidden block text-xs font-normal text-gray-600 dark:text-gray-500 hover:text-primary">{{ props.destination }}</a>
                 </div>
             </div>
-            <p class="text-sm font-medium text-red-600">{{ props.tag }}</p>
         </div>
-        <p class="my-6 text-sm font-normal text-gray-500">{{ props.description }}</p>
-        <div class="mt-6 flex items-center justify-between text-sm font-semibold text-gray-900">
+        <p class="my-6 text-sm font-normal text-dark dark:text-gray-500">{{ props.description }}</p>
+        <div class="mt-6 flex items-center justify-between text-sm font-semibold text-gray-900 dark:text-light">
             <div class="flex">
                 <TotalIcon class="size-4 mr-2"/>
-                <span class="mr-1">40</span> Task
+                <span class="mr-1">{{ props.tag }}</span> Tag
             </div>
             <div class="flex items-center">
                 <LinkIcon class="size-4 mr-2"/> 750
--- src/views/ClientView.vue
+++ src/views/ClientView.vue
@@ -3,14 +3,15 @@ import CardLink from '@/components/view/client/CardLink.vue';
 import ClientHead from '@/components/view/client/ClientHead.vue';
 
 const links = [
-  { short: 'https://shotlink.com/u3h4b5', destination: 'https://www.youtube.com/watch?v=rwIFN450NUQ&list=PLb9gJ6hvvBcdon6FkVS5oavn6vQy134zv&index=6', description: 'Spring Authorization Server es un framework que implementa las especificaciones OAuth 2.1 y OpenID Connect 1.0 y otras especificaciones relacionadas. Está construido sobre Spring Security para.', tag: '45' },
-  { short: 'https://shotlink.com/u3h4b5', destination: 'https://www.youtube.com/watch?v=rwIFN450NUQ&list=PLb9gJ6hvvBcdon6FkVS5oavn6vQy134zv&index=6', description: 'Spring Authorization Server es un framework que implementa las especificaciones OAuth 2.1 y OpenID Connect 1.0 y otras especificaciones relacionadas. Está construido sobre Spring Security para.', tag: '45' },
-  { short: 'https://shotlink.com/u3h4b5', destination: 'https://www.youtube.com/watch?v=rwIFN450NUQ&list=PLb9gJ6hvvBcdon6FkVS5oavn6vQy134zv&index=6', description: 'Spring Authorization Server es un framework que implementa las especificaciones OAuth 2.1 y OpenID Connect 1.0 y otras especificaciones relacionadas. Está construido sobre Spring Security para.', tag: '45' },
-  { short: 'https://shotlink.com/u3h4b5', destination: 'https://www.youtube.com/watch?v=rwIFN450NUQ&list=PLb9gJ6hvvBcdon6FkVS5oavn6vQy134zv&index=6', description: 'Spring Authorization Server es un framework que implementa las especificaciones OAuth 2.1 y OpenID Connect 1.0 y otras especificaciones relacionadas. Está construido sobre Spring Security para.', tag: '45' },
-  { short: 'https://shotlink.com/u3h4b5', destination: 'https://www.youtube.com/watch?v=rwIFN450NUQ&list=PLb9gJ6hvvBcdon6FkVS5oavn6vQy134zv&index=6', description: 'Spring Authorization Server es un framework que implementa las especificaciones OAuth 2.1 y OpenID Connect 1.0 y otras especificaciones relacionadas. Está construido sobre Spring Security para.', tag: '45' },
-  { short: 'https://shotlink.com/u3h4b5', destination: 'https://www.youtube.com/watch?v=rwIFN450NUQ&list=PLb9gJ6hvvBcdon6FkVS5oavn6vQy134zv&index=6', description: 'Spring Authorization Server es un framework que implementa las especificaciones OAuth 2.1 y OpenID Connect 1.0 y otras especificaciones relacionadas. Está construido sobre Spring Security para.', tag: '45' },
-  { short: 'https://shotlink.com/u3h4b5', destination: 'https://www.youtube.com/watch?v=rwIFN450NUQ&list=PLb9gJ6hvvBcdon6FkVS5oavn6vQy134zv&index=6', description: 'Spring Authorization Server es un framework que implementa las especificaciones OAuth 2.1 y OpenID Connect 1.0 y otras especificaciones relacionadas. Está construido sobre Spring Security para.', tag: '45' },
+  { short: 'https://shotlink.com/u3h4b5', destination: 'https://www.youtube.com/watch?v=rwIFN450NUQ&list=PLb9gJ6hvvBcdon6FkVS5oavn6vQy134zv&index=6', description: 'Spring Authorization Server es un framework que implementa las especificaciones OAuth 2.1.', tag: '45' },
+  { short: 'https://shotlink.com/u3h4b5', destination: 'https://www.youtube.com/watch?v=rwIFN450NUQ&list=PLb9gJ6hvvBcdon6FkVS5oavn6vQy134zv&index=6', description: 'Spring Authorization Server es un framework que implementa las especificaciones OAuth 2.1.', tag: '45' },
+  { short: 'https://shotlink.com/u3h4b5', destination: 'https://www.youtube.com/watch?v=rwIFN450NUQ&list=PLb9gJ6hvvBcdon6FkVS5oavn6vQy134zv&index=6', description: 'Spring Authorization Server es un framework que implementa las especificaciones OAuth 2.1.', tag: '45' },
+  { short: 'https://shotlink.com/u3h4b5', destination: 'https://www.youtube.com/watch?v=rwIFN450NUQ&list=PLb9gJ6hvvBcdon6FkVS5oavn6vQy134zv&index=6', description: 'Spring Authorization Server es un framework que implementa las especificaciones OAuth 2.1.', tag: '45' },
+  { short: 'https://shotlink.com/u3h4b5', destination: 'https://www.youtube.com/watch?v=rwIFN450NUQ&list=PLb9gJ6hvvBcdon6FkVS5oavn6vQy134zv&index=6', description: 'Spring Authorization Server es un framework que implementa las especificaciones OAuth 2.1.', tag: '45' },
+  { short: 'https://shotlink.com/u3h4b5', destination: 'https://www.youtube.com/watch?v=rwIFN450NUQ&list=PLb9gJ6hvvBcdon6FkVS5oavn6vQy134zv&index=6', description: 'Spring Authorization Server es un framework que implementa las especificaciones OAuth 2.1.', tag: '45' },
+  { short: 'https://shotlink.com/u3h4b5', destination: 'https://www.youtube.com/watch?v=rwIFN450NUQ&list=PLb9gJ6hvvBcdon6FkVS5oavn6vQy134zv&index=6', description: 'Spring Authorization Server es un framework que implementa las especificaciones OAuth 2.1.', tag: '45' },
 ]
+
 </script>
 
 <template>
@@ -19,7 +20,7 @@ const links = [
     <div class="mx-auto">
       <main class="">
         <div class="px-4">
-          <div class="grid sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3">
+          <div class="grid sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 2xl:grid-cols-4">
             <CardLink v-for="(item, index) in links" :key="index" :short="item.short" :destination="item.destination" :description="item.description" :tag="item.tag"/>
           </div>
         </div>
