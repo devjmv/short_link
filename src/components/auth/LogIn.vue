5d3d294cd74a0190e9e942d63258de74cf86c402
devjmv <javier161890@gmail.com>
2024-10-06 01:32:56 +0200

    added registerForm and updated loginForm

--- src/components/auth/LogIn.vue
+++ src/components/auth/LogIn.vue
@@ -4,6 +4,7 @@ import LoginForm from './LoginForm.vue'
 import { ModalStore } from '@/stores/modals/ModalStore'
 import { useAuthStore } from '@/stores/auth/auth'
 import { logoutStore } from '@/stores/auth/logout';
+import RegisterForm from './RegisterForm.vue';
 
 const modal = ModalStore()
 const auth = useAuthStore()
@@ -14,8 +15,12 @@ const logout = logoutStore()
 <template>
     <div v-if="!auth.user.isAuthenticated" class="hidden lg:flex lg:flex-1 lg:justify-end">
         <button @click="modal.open('login')"
-            class="mt-2 p-2 rounded-md ring-1 ring-inset ring-gray-500/10 flex items-center text-sm text-dark dark:text-gray-500 dark:hover:text-dark hover:text-white hover:bg-dark dark:hover:bg-gray-50">
+            class="mt-2 p-2 rounded-md ring-1 ring-inset ring-gray-500/10 flex items-center text-sm text-dark dark:text-gray-50 dark:hover:text-dark hover:text-white hover:bg-dark dark:hover:bg-gray-50">
             Log in <span aria-hidden="true">&rarr;</span>
+        </button>
+        <button @click="modal.open('register')"
+            class="mt-2 p-2 ml-2 rounded-md ring-1 ring-inset ring-gray-500/10 flex items-center text-sm text-dark dark:text-gray-50 dark:hover:text-dark hover:text-white hover:bg-dark dark:hover:bg-gray-50">
+            Register
         </button>
     </div>
 
@@ -41,6 +46,28 @@ const logout = logoutStore()
                         leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
                         <DialogPanel class="relative transform overflow-hidden rounded-lg shadow-xl transition-all ">
                             <LoginForm />
+                        </DialogPanel>
+                    </TransitionChild>
+                </div>
+            </div>
+        </Dialog>
+    </TransitionRoot>
+    <TransitionRoot as="login" :show="modal.openRegister">
+        <Dialog class="relative z-10" @close="modal.close()">
+            <TransitionChild as="login" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
+                leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+                <div class="fixed inset-0 bg-bgdark bg-opacity-75 transition-opacity" />
+            </TransitionChild>
+
+            <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center">
+                    <TransitionChild as="login" enter="ease-out duration-300"
+                        enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
+                        leave-from="opacity-100 translate-y-0 sm:scale-100"
+                        leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+                        <DialogPanel class="relative transform overflow-hidden rounded-lg shadow-xl transition-all ">
+                            <RegisterForm />
                         </DialogPanel>
                     </TransitionChild>
                 </div>
--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -10,38 +10,45 @@ const modal = ModalStore()
 
 const email = ref('')
 const password = ref('')
+const passwordAgain = ref('')
 const textAlert = ref('')
 
-async function login() {
-    if (email.value != '' && password.value != '')
-        try {
-            const response = await store.login(email.value, password.value)
+async function register() {
+    if (password.value !== '' && email.value !== '') {
+        if (password.value === passwordAgain.value) {
+            try {
+                const response = await store.register(email.value, password.value);
 
-            if (response.role) {
-                store.user.role = response['role']
-                store.user.isAuthenticated = true
-                store.user.access_token = response['access_token']
-                store.user.refresh_token = response['refresh_token']
+                if (response.role) {
+                    store.user.role = response['role']
+                    store.user.isAuthenticated = true
+                    store.user.access_token = response['access_token']
+                    store.user.refresh_token = response['refresh_token']
 
-                localStorage.setItem('role', response['role'])
-                localStorage.setItem('isAuthenticated', "true")
-                localStorage.setItem('access_token', response['access_token'])
-                localStorage.setItem('refresh_token', response['refresh_token'])
+                    localStorage.setItem('role', response['role'])
+                    localStorage.setItem('isAuthenticated', "true")
+                    localStorage.setItem('access_token', response['access_token'])
+                    localStorage.setItem('refresh_token', response['refresh_token'])
 
-                email.value = '';
-                password.value = '';
-                textAlert.value = '';
+                    textAlert.value = '';
+                    password.value = '';
+                    passwordAgain.value = '';
+                    email.value = '';
 
-                modal.close()
-                router.push('/client')
+                    modal.close()
+                    router.push('/client')
+                } else {
+                    textAlert.value = "The email already exists!";
+                }
+            } catch (error) {
+                textAlert.value = "Error trying to register, please try again.";
             }
-            else
-                textAlert.value = "Incorrect Email or password!";
-        } catch (error) {
-            textAlert.value = "Error trying to login, please try again.";
+        } else {
+            textAlert.value = "Passwords do not match!";
         }
-    else
+    } else {
         textAlert.value = "The email and password can't be empty!";
+    }
 }
 
 </script>
@@ -49,9 +56,9 @@ async function login() {
     <div class="w-full max-w-sm p-8 px-12 bg-light rounded-md shadow-md">
         <div class="flex items-center justify-center">
             <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-            <span class="font-gloria text-2xl ml-3">LogIn to Short Link</span>
+            <span class="font-gloria text-2xl ml-3">Register in Short Link</span>
         </div>
-        <form class="space-y-6" @submit.prevent="login">
+        <form class="space-y-6" @submit.prevent="register">
 
             <div v-if="textAlert != ''"
                 class="mt-4 font-regular relative block w-full rounded-lg bg-red-500 p-4 text-base leading-5 text-white opacity-100"
@@ -75,37 +82,26 @@ async function login() {
             </div>
 
             <div>
+                <div class="mt-2">
+                    <input v-model="passwordAgain" id="confirmPassword" name="confirmPassword" type="password"
+                        autocomplete="current-confirmPassword" placeholder="Confirm Password"
+                        class="block w-full rounded-md border-0 py-2 px-2 text-gray-900 shadow-sm placeholder:text-gray-400 focus:outline-primary sm:text-sm sm:leading-6" />
+                </div>
+            </div>
+
+            <div>
                 <button type="submit"
                     class="w-full px-4 py-2 text-sm text-center text-white bg-primary rounded-md focus:outline-none hover:bg-secondary hover:text-dark">
-                    Sign in
+                    Register
                 </button>
             </div>
 
         </form>
         <div>
-            <button @click="modal.close(), modal.open('register')"
+            <button @click="modal.close(), modal.open('login')"
                 class="mt-2 w-full px-4 py-2 text-sm text-center text-white bg-primary rounded-md focus:outline-none hover:bg-secondary hover:text-dark">
-                Register
+                Sign in
             </button>
         </div>
-        <form class="mt-4">
-            <div class="flex items-center justify-center mt-4">
-                <p class="block text-sm text-verdigris fontme" href="#">Or log in with your favorite social provider to
-                    get
-                    started:</p>
-            </div>
-            <div class="mt-6">
-                <button type="submit"
-                    class="w-full px-4 py-2 text-sm text-center text-white bg-primary rounded-md focus:outline-none hover:bg-secondary hover:text-dark">
-                    Google
-                </button>
-            </div>
-            <div class="mt-2">
-                <button type="submit"
-                    class="w-full px-4 py-2 text-sm text-center text-white bg-primary rounded-md focus:outline-none hover:bg-secondary hover:text-dark">
-                    GitHub
-                </button>
-            </div>
-        </form>
     </div>
 </template>
