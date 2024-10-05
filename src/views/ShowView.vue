8e95fbad6f213ad122d05597bc0764a887f63653
devjmv <javier161890@gmail.com>
2024-10-05 22:46:37 +0200

    save link social

---
+++ src/views/ShowView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div></div>
+    <!-- <p>
+    <a class="snb snbfacebook" href="#"
+      onclick="window.open('https:\/\/www.facebook.com/sharer/sharer.php?u=shorturl.at/pNSYL', 'facebook', 'width=800, height=600, resizable, scrollbars=yes, status=1');">Facebook</a>
+    <a class="snb snbtwitter" href="#"
+      onclick="window.open('https:\/\/twitter.com/share?url=shorturl.at/pNSYL', 'twitter', 'width=800, height=600, resizable, scrollbars=yes, status=1');">X</a>
+    <a class="snb snbpinterest" href="#"
+      onclick="window.open('https:\/\/pinterest.com/pin/create/link/?url=shorturl.at/pNSYL', 'pinterest', 'width=800, height=600, resizable, scrollbars=yes, status=1');">Pinterest</a>
+    <a class="snb snbwhatsapp" href="whatsapp://send?text=shorturl.at/pNSYL">WhatsApp</a>
+    <a class="snb snbtelegram" href="#"
+      onclick="window.open('https:\/\/telegram.me/share/url?url=shorturl.at/pNSYL', 'telegram', 'width=800, height=600, resizable, scrollbars=yes, status=1');">Telegram</a>
+    <a class="snb snbreddit" href="#"
+      onclick="window.open('https:\/\/www.reddit.com/submit?url=shorturl.at/pNSYL', 'reddit', 'width=800, height=600, resizable, scrollbars=yes, status=1');">Reddit</a>
+    <a class="snb snbtumblr" href="#"
+      onclick="window.open('https:\/\/www.tumblr.com/share/link?url=shorturl.at/pNSYL', 'tumblr', 'width=800, height=600, resizable, scrollbars=yes, status=1');">Tumblr</a>
+    <a class="snb snblinkedin" href="#"
+      onclick="window.open('http:\/\/www.linkedin.com/shareArticle?mini=true&amp;url=shorturl.at/pNSYL', 'linkedin', 'width=800, height=600, resizable, scrollbars=yes, status=1');">LinkedIn</a>
+  </p> -->
+</template>
