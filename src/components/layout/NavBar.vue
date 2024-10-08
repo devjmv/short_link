69d70d5383c15b64f975ac756198e312693da2d3
devjmv <javier161890@gmail.com>
2024-10-08 11:22:05 +0200

    added icon app

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -57,7 +57,7 @@ const mobileMenuOpen = ref(false)
         <div class="flex lg:flex-1">
             <a href="#" class="-m-1.5 p-1.5">
                 <span class="sr-only">ShortLink</span>
-                <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                <img class="h-8 w-auto" src="../../assets/_deb40a7e-3ce1-4ad4-be7c-24685da10a0e-removebg-preview.png" alt="" />
             </a>
         </div>
         <PopoverGroup class="hidden lg:flex lg:gap-x-2">
